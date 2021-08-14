7a2f68f2111777def28dea47a8e3f3b8cdad92ee
rmarfatia <rmarfatia@pinterest.com>
2021-08-13 20:49:46 -0700

    Adding App Dependencies

---
+++ app/src/main/java/com/rikin/dispochallenge/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.rikin.dispochallenge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rikin.dispochallenge.ui.theme.DispoChallengeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DispoChallengeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DispoChallengeTheme {
+        Greeting("Android")
+    }
+}
