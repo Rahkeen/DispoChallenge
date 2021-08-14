b973ff6516fc1e7906ea416f72d224fd15213771
rmarfatia <rmarfatia@pinterest.com>
2021-08-13 21:28:15 -0700

    Added Giphy API + Models

--- app/src/main/java/com/rikin/dispochallenge/MainActivity.kt
+++ app/src/main/java/com/rikin/dispochallenge/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rikin.dispochallenge
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,9 +9,16 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.rikin.dispochallenge.ui.theme.DispoChallengeTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +30,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    val scope = rememberCoroutineScope()
+                    scope.launch(Dispatchers.IO) {
+                        val gifs = Networking.client.getGifs(searchQuery = "Hello")
+                        Log.d("GiphyService", gifs.data[0].title)
+                    }
                     Greeting("Android")
                 }
             }
---
+++ app/src/main/java/com/rikin/dispochallenge/Networking.kt
@@ -0,0 +1,55 @@
+package com.rikin.dispochallenge
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://api.giphy.com/v1/gifs/"
+private const val API_KEY = "74IXqjiV8RuLA2yZT2EuloLW9mHhIlna"
+
+object Networking {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create())
+        .build()
+
+    val client = retrofit.create(GiphyService::class.java)
+}
+
+interface GiphyService {
+
+    @GET("search")
+    suspend fun getGifs(
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("q") searchQuery: String
+    ) : GiphyGifs
+}
+
+@JsonClass(generateAdapter = true)
+data class GiphyGifs(
+    val data: List<GiphyGif>
+)
+
+@JsonClass(generateAdapter = true)
+data class GiphyGif(
+    @Json(name = "title") val title: String,
+    @Json(name = "images") val images: GiphyImages,
+    @Json(name = "rating") val rating: String,
+    @Json(name = "url") val url: String
+)
+
+@JsonClass(generateAdapter = true)
+data class GiphyImages(
+    @Json(name = "original") val original: GiphyImage,
+    @Json(name = "original_still") val originalStill: GiphyImage
+)
+
+@JsonClass(generateAdapter = true)
+data class GiphyImage(
+    @Json(name = "width") val width: Int,
+    @Json(name = "height") val height: Int,
+    @Json(name = "url") val url: String,
+)
